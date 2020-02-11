bcdd6995f4d192716a95777281d8c66a7998f4eb
Yana Stepanova / mansuedei <mansuedei@yandex.ru>
2020-02-11 14:54:04 +0300

    removed src from build

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import "react-app-polyfill/ie9";
-import "react-app-polyfill/stable";
-
-import React from "react";
-import ReactDOM from "react-dom";
-import "../src/styles/index.css";
-import App from "./App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
