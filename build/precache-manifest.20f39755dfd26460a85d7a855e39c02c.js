ec256e1ae31683680fe0b0b4d11651bb6cdf2736
Yana Stepanova / mansuedei <mansuedei@yandex.ru>
2020-02-11 14:49:58 +0300

    build

---
+++ build/precache-manifest.20f39755dfd26460a85d7a855e39c02c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b70f8e1c35813385b81f857deef3cbd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "84f104bbd61c32ddee38",
+    "url": "./static/css/main.abbf59d0.chunk.css"
+  },
+  {
+    "revision": "a71744b6d3ebd56e47c1",
+    "url": "./static/js/2.0c192de5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.0c192de5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84f104bbd61c32ddee38",
+    "url": "./static/js/main.d23b185b.chunk.js"
+  },
+  {
+    "revision": "f14ff0fb2249c0b9ebc0",
+    "url": "./static/js/runtime-main.56c684d9.js"
+  },
+  {
+    "revision": "5de5151abc34eda30a78eb824338404b",
+    "url": "./static/media/el.5de5151a.png"
+  }
+]);
